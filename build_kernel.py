b4a2575e01ac97d3c6e65bb2a821b4cdf6b2853e
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2012-10-14 23:16:53 +0400

    Huge refactoring.

--- build_kernel.py
+++ build_kernel.py
@@ -1,48 +1,60 @@
 #!/usr/bin/python
 import os
 import sys
-from itertools import ifilter
+import json
 import subprocess
-from shutil import copyfile
+from os.path import join
+from itertools import ifilter
+from shutil import copyfile as _copyfile
 from optparse import OptionParser
-from ConfigParser import RawConfigParser
 
-DEFAULT_CONF = "/etc/build_kernel.conf"
+DEFAULT_CONF_PATH = '/etc/build_kernel.conf'
+DEFAULT_CONF = {
+    'arch' : 'i386',
+    'src_linux' : '/usr/src/linux',
+    'boot_path' : '/boot',
+    'mbr_hdd' : '/dev/sda',
+    'grub_conf_path' : '/boot/grub.conf',
+    'remount_boot' : False,
+    'max_kernels' : 5, 
+    'boot_params' : ''
+}
 
-conf = None
-
-def load_conf(conf_path=DEFAULT_CONF):
-    global conf
-    conf = RawConfigParser()
-    conf.read(conf_path)
+def load_conf(conf_path=DEFAULT_CONF_PATH):
+    conf = DEFAULT_CONF.copy()
+    if os.path.exists(conf_path):
+        conf.update(json.load(open(conf_path)))
     return conf
 
 def main():
-    exit_if_user_is_not_root()
     options = parse_cli_options()
+    exit_if_user_is_not_root()    
+    # TODO remove check for file when it's optional
     conf_path = os.path.abspath(options.conf_path)
     if not os.path.isfile(conf_path):
-        print "ERROR: wrong build_kernel.conf file : ", conf_path
+        print >> sys.stderr, "ERROR: wrong build_kernel.conf file : ", conf_path
         sys.exit()
     print "using conf from : ", conf_path
-    load_conf(conf_path)
-    compile_kernel(options.linux_config_path)
-    install_kernel(get_kernel_version())
-    grub_conf = load_grub_conf()
-    #print grub_conf
-    if not is_in_grub_conf(grub_conf, get_kernel_version()):
+    process(load_conf(conf_path), options.force_version)
+
+def process(conf, force_version=None):    
+    compile_kernel(conf['src_linux'])
+    kernel_version = force_version or extract_version_from_src_path(conf['src_linux'])
+    install_kernel(kernel_version, conf)
+    grub_conf = load_grub_conf(conf['grub_conf_path'])
+    if not is_in_grub_conf(grub_conf, kernel_version):
         print " kernel not found in grub.conf -> adding"
-        removed_kernels = add_to_grub_conf(grub_conf, get_kernel_version())
+        removed_kernels = add_to_grub_conf_and_remove_if_needed(grub_conf, kernel_version, conf)
         save_grub_conf(grub_conf, conf)
-        remove_old_kernels(prepare_remove_kernels(removed_kernels))
+        remove_old_kernels(prepare_remove_kernels(removed_kernels, conf), conf)
     else:
         print " kernel found in grub.conf"
-    run_external_tool()
+    run_external_tool(conf['external_tool'])
 
 def parse_cli_options():
     parser = OptionParser()
     parser.add_option("-c", "--conf", dest="conf_path", default=DEFAULT_CONF, help="Path to build_kernel.conf file (default:%s)" % DEFAULT_CONF, metavar="FILE")
-    parser.add_option("-C", "--config", dest="linux_config_path", default=None, help="Path to .config file", metavar="FILE")
+    parser.add_option("-v", "--force-version", dest="force_version", help="Force to use given version")
     options, dummy = parser.parse_args()
     return options
 
@@ -51,22 +63,23 @@ def exit_if_user_is_not_root():
         print >> sys.stderr, "You must be root to run this script."
         sys.exit(1)
 
-def get_kernel_version():
-    return os.path.basename(os.path.realpath(conf.get('main', 'src_linux')))
+def extract_version_from_src_path(kernel_src_path):
+    return os.path.basename(os.path.realpath(kernel_src_path))
 
-def get_kernel_path(kernel_version):
+def get_kernel_path(boot_path, kernel_version):
     if kernel_version:
-        return os.path.join(conf.get('main', 'boot_path'), 'kernel-%s' % kernel_version)
+        return os.path.join(boot_path, 'kernel-%s' % kernel_version)
     else:
-        return os.path.join(conf.get('main', 'boot_path'), 'kernel')
+        return os.path.join(boot_path, 'kernel')
 
-def get_system_map_path(image):
-    temp = image.rpartition(os.sep)[2].rpartition("kernel-")
-    if temp[1]:
-        system_map = os.path.join(conf.get('main', 'boot_path'), 'System.map-%s' % temp[2])
+def get_system_map_path(boot_path, kernel_version):    
+    if kernel_version:
+        return os.path.join(boot_path, 'System.map-%s' % kernel_version)
     else:
-        system_map = os.path.join(conf.get('main', 'boot_path'), 'System.map')
-    return system_map
+        return os.path.join(boot_path, 'System.map')
+
+def get_system_map_path_from_kernel(boot_path, kernel):
+    return get_system_map_path(boot_path, kernel.rpartition(os.sep)[2].rpartition("kernel-")[2])
 
 def backup_file(path, symbol='~'):
     if os.path.exists(path):
@@ -77,76 +90,69 @@ def restore_file(path, symbol='~'):
     if os.path.exists(backup):
         copyfile(backup, path)
 
-def compile_kernel(linux_config_path=None):
-    print "compiling kernel...", get_kernel_version()
-    if linux_config_path:
-        linux_config = os.path.join(conf.get('main', 'src_linux'), '.config')
-        backup_file(linux_config)
-        copyfile(linux_config_path, linux_config)
-    subprocess.call(["make", ], cwd=conf.get('main', 'src_linux'))
-    subprocess.call(["make", "modules_install"], cwd=conf.get('main', 'src_linux'))
+def compile_kernel(kernel_src_path):
+    kernel_version = extract_version_from_src_path(kernel_src_path)
+    print "compiling kernel...", kernel_version
+    subprocess.call(["make", ], cwd=kernel_src_path)
+    subprocess.call(["make", "modules_install"], cwd=kernel_src_path)
     print "compiling kernel...Ok"
 
-def install_kernel(kernel_version):
+def install_kernel(kernel_version, conf):
     print "installing kernel...", kernel_version
-    _remount_boot_for_write()
-    copyfile(os.path.join(conf.get('main', 'src_linux'), 'arch', conf.get('main', 'arch'), 'boot/bzImage'), get_kernel_path(kernel_version))
-    copyfile(os.path.join(conf.get('main', 'src_linux'), "System.map"), os.path.join(conf.get('main', 'boot_path'), 'System.map-%s' % kernel_version))
-    _remount_boot_for_read()
+    _remount_boot_for_write(conf['remount_boot'])
+    copyfile(join(conf['src_linux'], 'arch', conf['arch'], 'boot/bzImage'), get_kernel_path(conf['boot_path'], kernel_version))
+    copyfile(join(conf['src_linux'], 'System.map'), get_system_map_path(conf['boot_path'], kernel_version))
+    _remount_boot_for_read(conf['remount_boot'])
     print "installing kernel...Ok"
+
+def load_grub_conf(from_path):
+    print "loading grub.conf... ", from_path
+    boot = None
+    boots = []
+    params = []
+    with open(from_path, "rt") as fin:
+        content = fin.read()    
+        for line in ifilter(lambda line: line, content.split("\n")):
+            if "title=" in line:
+                boot = [line]
+                boots.append(boot)
+            elif boot:
+                boot.append(line)
+            else:
+                params.append(line)
+    result = { "params" : params, "boot" : boots}
+    print "loading grub.conf...Ok (%s kernels found)" % len(result["boot"])
+    return result
 
 def is_in_grub_conf(grub_conf, kernel_version):
     print ' checking for kernel...', kernel_version
     for boot in grub_conf["boot"]:
         if len(filter(lambda line: "title=" in line and line.partition("=")[2].strip() == kernel_version, boot)):
-            return True
+            return True    
 
-def load_grub_conf():
-    print "loading grub.conf... ", conf.get('main', 'grub_conf')
-    fin = open(conf.get('main', 'grub_conf'), "rt")
-    content = fin.read()
-    boot = None
-    boots = []
-    params = []
-    for line in ifilter(lambda line: line, content.split("\n")):
-        #print "line : ", line
-        if "title=" in line:
-            #title = line.partition("=")[2].strip()
-            #print 'title :', title
-            boot = [line]
-            boots.append(boot)
-        elif boot:
-            boot.append(line)
-        else:
-            params.append(line)
-    result = { "params" : params, "boot" : boots}
-    fin.close()
-    print "loading grub.conf...Ok (%s kernels found)" % len(result["boot"])
-    return result
-
-def save_grub_conf(grub_conf, conf=conf):
-    print " saving grub.conf...", conf.get('main', 'grub_conf')
-    _remount_boot_for_write()
+def save_grub_conf(grub_conf, conf):
+    print " saving grub.conf...", conf['grub_conf_path']
+    _remount_boot_for_write(conf['remount_boot'])
     print "  backuping grub.conf..."
-    backup_file(conf.get('main', 'grub_conf'))
+    backup_file(conf['grub_conf_path'])
     print "  backuping grub.conf...Ok"
-    fout = open(conf.get('main', 'grub_conf'), "wt")
-    for param in grub_conf["params"]:
-        fout.write(param + "\n\n")
-    fout.write("\n")
-    for conf in grub_conf["boot"]:
-    #print 'conf :', conf
-        for line in conf:
-            fout.write(line + "\n")
+    with open(conf['grub_conf_path'], "wt") as fout:
+        for param in grub_conf["params"]:
+            fout.write(param + "\n\n")
         fout.write("\n")
-    fout.close()
-    _remount_boot_for_read()
+        for el in grub_conf["boot"]:
+            for line in el:
+                fout.write(line + "\n")
+            fout.write("\n")
+    _remount_boot_for_read(conf['remount_boot'])
     print " saving grub.conf...Ok"
 
-def add_to_grub_conf(grub_conf, kernel_version):
+def add_to_grub_conf_and_remove_if_needed(grub_conf, kernel_version, conf):
+    # TODO split and simplify this method
     print "adding to grub.conf kernel...", kernel_version
-    kernel_string = 'kernel %s root=%s %s' % (get_kernel_path(kernel_version), conf.get('main', 'root_partition'), conf.get('main', 'boot_params'))
-    grub_conf["boot"].insert(0, ["title=%s" % kernel_version, "root (%s)" % conf.get('main', 'boot_partition_grub'), kernel_string])
+    kernel_string = 'kernel %s root=%s %s' % (get_kernel_path(conf['boot_path'], kernel_version), 
+        conf['root_partition'], conf['boot_params'])
+    grub_conf["boot"].insert(0, ["title=%s" % kernel_version, "root (%s)" % conf['boot_partition_grub'], kernel_string])
     removed_kernels = []
 
     gentoo_count = 0
@@ -156,7 +162,7 @@ def add_to_grub_conf(grub_conf, kernel_version):
         removed_kernel = grub_conf["boot"][i]
         if "gentoo" in removed_kernel[0].lower():
             gentoo_count = gentoo_count + 1
-            if gentoo_count > conf.getint('main', 'max_kernels'):
+            if gentoo_count > conf['max_kernels']:
                 print "   removing old kernel from list : ", removed_kernel
                 del grub_conf["boot"][i]
                 j = j - 1
@@ -168,48 +174,52 @@ def add_to_grub_conf(grub_conf, kernel_version):
     print "adding to grub.conf kernel...Ok (%s old kernel removed)" % len(removed_kernels)
     return removed_kernels
 
-def prepare_remove_kernels(removed_kernels):
+def prepare_remove_kernels(removed_kernels, conf):
     result = []
     for kernel in removed_kernels:
         kernel_version = None
         for line in kernel:
             if line.startswith("title"):
                 kernel_version = line.partition("=")[2]
-            if line.startswith("kernel") and kernel_version:
-                image = line.split(" ")[1]
-                system_map = get_system_map_path(image)
+            if line.startswith("kernel") and kernel_version:                
+                image = os.path.join(conf['boot_path'], line.split(" ")[1].partition(os.sep)[2])
+                system_map = get_system_map_path_from_kernel(conf['boot_path'], image)
                 result.append((image, system_map))
     return result
 
-def remove_old_kernels(removed_kernels):
+def remove_old_kernels(removed_kernels, conf):
     print "removing old kernels from disk..."
     for kernel in removed_kernels:
         image = kernel[0]
         system_map = kernel[1]
-        _remount_boot_for_write()
+        _remount_boot_for_write(conf['remount_boot'])
         if os.path.exists(image):
             print " deleting kernel image : ", image
             os.remove(image)
         if os.path.exists(system_map):
             print " deleting kernel map : ", system_map
             os.remove(system_map)
-        _remount_boot_for_read()
+        _remount_boot_for_read(conf['remount_boot'])
     print "removing old kernels from disk...Ok"
 
-def run_external_tool():
-    print "running needed external tools...", conf.get('main', 'external_tool')
-    subprocess.call(conf.get('main', 'external_tool').split(' '))
+def run_external_tool(external_tools):
+    print "running needed external tools...", external_tools
+    subprocess.call(external_tools.split(' '))
     print "running needed external tools...Ok"
 
-def _remount_boot_for_write():
-    if conf.getboolean('main', 'remount_boot'):
+def _remount_boot_for_write(remount_boot):
+    if remount_boot:
         subprocess.call(["umount", conf.get('main', 'boot_path')])
         subprocess.call(["mount", conf.get('main', 'boot_partition'), conf.get('main', 'boot_path')])
 
-def _remount_boot_for_read():
-    if conf.getboolean('main', 'remount_boot'):
+def _remount_boot_for_read(remount_boot):
+    if remount_boot:
         subprocess.call(["umount", conf.get('main', 'boot_path')])
         subprocess.call(["mount", conf.get('main', 'boot_path')])
+
+def copyfile(src, dst):
+    print '  copying from %s to %s' % (src, dst)
+    return _copyfile(src, dst)
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -5,151 +5,182 @@
 import unittest
 import os
 import sys
+import tempfile
+
+from shutil import rmtree, copyfile, copytree
+from mock import patch
 
 import build_kernel
 
-TEST_CONF = os.path.join(os.path.dirname(__file__), 'test', 'build_kernel.conf.test')
-TEST_VERSION = 'test-kernel'
-TEST_GENTOO_VERSION = 'gentoo-kernel'
+TEST_CONF = os.path.join(os.path.dirname(__file__), 'test/etc/build_kernel.conf.json')
 
-KERNELS_IN_GRUB = 5
-
-class Test(unittest.TestCase):
+class BuildKernelTest(unittest.TestCase):
 
     def setUp(self):
         sys.path.insert(0, os.path.dirname(__file__))
-        build_kernel.conf = build_kernel.load_conf(TEST_CONF)
+        self.temp_dir = tempfile.mkdtemp('build_kernel_test')
+        copytree('test/boot', os.path.join(self.temp_dir, 'boot'))
 
-    def test_load_config(self):
-        conf = build_kernel.conf
+    def tearDown(self):
+        rmtree(self.temp_dir)
+
+    def test_load_config_should_not_fail_if_config_path_is_wrong(self):
+        # given & when
+        conf = build_kernel.load_conf('some wrong path')
+        # then
+        self.assertEqual(conf['arch'], 'i386')
+        self.assertEqual(conf['src_linux'], '/usr/src/linux')
+        self.assertEqual(conf['boot_path'], '/boot')
+        self.assertEqual(conf['mbr_hdd'], '/dev/sda')
+        self.assertEqual(conf['max_kernels'], 5)
+        self.assertEqual(conf['boot_params'], "")        
+        self.assertFalse(conf['remount_boot'])
+
+    def test_load_config_should_not_fail(self):
+        conf = build_kernel.load_conf(TEST_CONF)
         self.assertTrue(conf is not None)
-        self.assertEqual(conf.get('main', 'src_linux'), '/usr/src/linux')
+        self.assertEqual(conf['arch'], 'amd64')
+        self.assertEqual(conf['src_linux'], 'test/usr/src/linux')
+        self.assertEqual(conf['boot_path'], 'test/boot')
+        self.assertEqual(conf['mbr_hdd'], '/dev/sda')
+        self.assertEqual(conf['max_kernels'], 5)
+        self.assertEqual(conf['boot_params'], "")        
+        self.assertFalse(conf['remount_boot'])
 
-    def test_compile_kernel(self):
-        pass
-        #build_kernel.compile_kernel()
+    @patch('build_kernel.subprocess')
+    def test_compile_kernel(self, subprocess_mock):
+        # given & when
+        build_kernel.compile_kernel('test/usr/src/linux')
+        # then
+        self.assertEqual(len(subprocess_mock.mock_calls), 2)
 
-    def test_install_kernel(self):
-        build_kernel.install_kernel(TEST_VERSION)
-        self.assertTrue(os.path.exists(build_kernel.get_kernel_path(TEST_VERSION)))
-        self.assertTrue(os.path.exists(build_kernel.get_system_map_path(build_kernel.get_kernel_path(TEST_VERSION))))
-        os.remove(build_kernel.get_kernel_path(TEST_VERSION))
-        os.remove(build_kernel.get_system_map_path(build_kernel.get_kernel_path(TEST_VERSION)))
+    def test_install_kernel_should_copy_kernel(self):
+        # given
+        conf = build_kernel.load_conf(TEST_CONF)
+        conf['boot_path'] = self.temp_dir
+        # when
+        build_kernel.install_kernel('test-v1', conf)
+        # then
+        self.assertEqual(len(os.listdir(self.temp_dir)), 3)
+        self.assertTrue(os.path.exists(os.path.join(self.temp_dir, 'kernel-test-v1')))
+        self.assertTrue(os.path.exists(os.path.join(self.temp_dir, 'System.map-test-v1')))
 
-    def test_load_grub_conf(self):
-        grub_conf = build_kernel.load_grub_conf()
+    def test_load_grub_conf_should_return_5_kernels(self):
+        # given & when
+        grub_conf = build_kernel.load_grub_conf('test/boot/grub.conf')
+        # then
         self.assertTrue(grub_conf is not None)
         self.assertTrue('boot' in grub_conf)
-        self.assertEqual(len(grub_conf['boot']), KERNELS_IN_GRUB)
+        self.assertEqual(len(grub_conf['boot']), 5)
 
-    def test_is_in_grub_conf(self):
-        grub_conf = build_kernel.load_grub_conf()
+    def test_is_in_grub_conf_should_work(self):
+        # given
+        grub_conf = build_kernel.load_grub_conf('test/boot/grub.conf')
+        # when & then
         self.assertTrue(build_kernel.is_in_grub_conf(grub_conf, 'linux-2.6.32-gentoo-r7'))
         self.assertFalse(build_kernel.is_in_grub_conf(grub_conf, 'linux-2.6.1'))
 
-    def test_add_to_grub_conf(self):
-        grub_conf = build_kernel.load_grub_conf()
-        self.assertEqual(len(grub_conf['boot']), KERNELS_IN_GRUB)
-        removed_kernels = build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_GENTOO_VERSION))
-        self.assertEqual(len(grub_conf['boot']), KERNELS_IN_GRUB + 1)
+    def test_add_to_grub_conf_and_remove_if_needed_should_work(self):
+        # TODO split this test
+        # given
+        grub_conf = build_kernel.load_grub_conf('test/boot/grub.conf')
+        conf = build_kernel.load_conf(TEST_CONF)
+        self.assertEqual(len(grub_conf['boot']), 5)
+        # when add one
+        removed_kernels = build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('gentoo-test-v1'), conf)
+        # then should +1
+        self.assertEqual(len(grub_conf['boot']), 6)
         self.assertEqual(len(removed_kernels), 0)
-        removed_kernels = build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_VERSION))
-        self.assertEqual(len(grub_conf['boot']), KERNELS_IN_GRUB + 2)
+        # when add one more
+        removed_kernels = build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('test-v1'), conf)
+        # then should +1
+        self.assertEqual(len(grub_conf['boot']), 7)
         self.assertEqual(len(removed_kernels), 0)
-        removed_kernels = build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_GENTOO_VERSION))
-        self.assertEqual(len(grub_conf['boot']), KERNELS_IN_GRUB + 2)
+        # when add one more
+        removed_kernels = build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('gentoo-test-v1'), conf)
+        # then len is not changed
+        self.assertEqual(len(grub_conf['boot']), 7)
+        #  because one is removed
         self.assertEqual(len(removed_kernels), 1)
-
-    def test_save_grub_conf(self):
-        grub_conf = build_kernel.load_grub_conf()
-        self.assertEqual(len(grub_conf['boot']), KERNELS_IN_GRUB)
-        build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_GENTOO_VERSION))
-        build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_VERSION))
-        build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_GENTOO_VERSION))
-        build_kernel.save_grub_conf(grub_conf, build_kernel.conf)
-        new_grub_conf = build_kernel.load_grub_conf()
-        self.assertEqual(len(new_grub_conf['boot']), KERNELS_IN_GRUB + 2)
-        build_kernel.restore_file(build_kernel.conf.get('main', 'grub_conf'))
-        new_grub_conf = build_kernel.load_grub_conf()
-        self.assertEqual(len(new_grub_conf['boot']), KERNELS_IN_GRUB)
-        os.remove(build_kernel.conf.get('main', 'grub_conf') + '~')
-
-    def test_remove_old_kernels(self):
-        grub_conf = build_kernel.load_grub_conf()
-        removed_kernels = build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_GENTOO_VERSION))
-        removed_kernels = removed_kernels + build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_VERSION))
-        removed_kernels = removed_kernels + build_kernel.add_to_grub_conf(grub_conf, get_unique_version(TEST_GENTOO_VERSION))
-        prepared_removed_kernels = build_kernel.prepare_remove_kernels(removed_kernels)
+    
+    def test_remove_old_kernels_should_work(self):
+        # given
+        grub_conf = build_kernel.load_grub_conf('test/boot/grub.conf')
+        conf = build_kernel.load_conf(TEST_CONF)
+        conf['boot_path'] = os.path.join(self.temp_dir, 'boot')
+        removed_kernels = build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('gentoo-test-v1'), conf) + \
+        build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('test-v1'), conf) + \
+        build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('gentoo-test-v1'), conf)
+        prepared_removed_kernels = build_kernel.prepare_remove_kernels(removed_kernels, conf)
+        print prepared_removed_kernels
         for kernel in prepared_removed_kernels:
             image = kernel[0]
             system_map = kernel[1]
-            build_kernel.backup_file(image)
-            build_kernel.backup_file(system_map)
             self.assertTrue(os.path.exists(image))
-            self.assertTrue(os.path.exists(system_map))
-        build_kernel.remove_old_kernels(prepared_removed_kernels)
+            self.assertTrue(os.path.exists(system_map))        
+        build_kernel.remove_old_kernels(prepared_removed_kernels, conf)
         for kernel in prepared_removed_kernels:
             image = kernel[0]
             system_map = kernel[1]
             self.assertFalse(os.path.exists(image))
             self.assertFalse(os.path.exists(system_map))
-        for kernel in prepared_removed_kernels:
-            image = kernel[0]
-            system_map = kernel[1]
-            build_kernel.restore_file(image)
-            build_kernel.restore_file(system_map)
-            self.assertTrue(os.path.exists(image))
-            self.assertTrue(os.path.exists(system_map))
-            os.remove(image + '~')
-            os.remove(system_map + '~')
 
-    def test_main(self):
-        # fix argv
-        sys.argv.append('-c')
-        sys.argv.append(TEST_CONF)
-        def stub(*args):
-            pass
-        build_kernel.exit_if_user_is_not_root = stub
-        build_kernel.compile_kernel = stub
-        build_kernel._update_grub = stub
-        build_kernel.run_external_tool = stub
-        def _test():
-            build_kernel.main()
-            image = build_kernel.get_kernel_path(build_kernel.get_kernel_version())
-            system_map = build_kernel.get_system_map_path(image)
+    def test_save_grub_conf_should_work(self):
+        # given
+        conf = build_kernel.load_conf(TEST_CONF)
+        conf['grub_conf_path'] = os.path.join(self.temp_dir, 'new-grub.conf')
+        grub_conf = build_kernel.load_grub_conf('test/boot/grub.conf')        
+        self.assertEqual(len(grub_conf['boot']), 5)
+        build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('gentoo-test-v1'), conf)
+        build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('test-v1'), conf)
+        build_kernel.add_to_grub_conf_and_remove_if_needed(grub_conf, 
+            get_unique_version('gentoo-test-v1'), conf)
+        self.assertEqual(len(grub_conf['boot']), 7)
+        # when
+        build_kernel.save_grub_conf(grub_conf, conf)
+        # then
+        new_grub_conf = build_kernel.load_grub_conf(os.path.join(self.temp_dir, 'new-grub.conf'))
+        self.assertEqual(len(new_grub_conf['boot']), 7)
+        # TODO add test for backup
+
+    @patch('build_kernel.subprocess')
+    def test_run_external_tool(self, subprocess_mock):        
+        build_kernel.run_external_tool('echo Test Ok')
+        self.assertEqual(len(subprocess_mock.mock_calls), 1)
+
+    @patch('build_kernel.subprocess')
+    def test_process_should_work(self, subprocess_mock):
+        # given
+        conf = build_kernel.load_conf(TEST_CONF)
+        #  hack conf here
+        conf['src_linux'] = 'test/usr/src/linux'
+        conf['arch'] = 'amd64'
+        conf['boot_path'] = os.path.join(self.temp_dir, 'boot')
+        conf['grub_conf_path'] = os.path.join(self.temp_dir, 'boot/grub.conf')
+        def test_process_for_given_version(version='linux'):
+            build_kernel.process(conf, version)
+            image = os.path.join(self.temp_dir, 'boot/kernel-%s' % version)
+            system_map = os.path.join(self.temp_dir, 'boot/System.map-%s' % version)
             self.assertTrue(os.path.exists(image), 'File not found %s' % image)
             self.assertTrue(os.path.exists(system_map), 'File not found %s' % system_map)
-            os.remove(image)
-            os.remove(system_map)
-            self.assertFalse(os.path.exists(image))
-            self.assertFalse(os.path.exists(system_map))
-        build_kernel.backup_file(build_kernel.conf.get('main', 'grub_conf'),'~~')
-        build_kernel.backup_file(build_kernel.get_kernel_path(None))
-        build_kernel.backup_file(build_kernel.get_system_map_path(build_kernel.get_kernel_path(None)))
-        _test()
-        # hack kernel version
-        version = get_unique_version(TEST_GENTOO_VERSION)
-        build_kernel.get_kernel_version = lambda: version
-        _test()
-        version = get_unique_version(TEST_GENTOO_VERSION)
-        build_kernel.get_kernel_version = lambda: version
-        _test()
-        build_kernel.restore_file(build_kernel.conf.get('main', 'grub_conf'),'~~')
-        build_kernel.restore_file(build_kernel.get_kernel_path(None))
-        build_kernel.restore_file(build_kernel.get_system_map_path(build_kernel.get_kernel_path(None)))
-        os.remove(build_kernel.get_kernel_path(None) + '~')
-        os.remove(build_kernel.get_system_map_path(build_kernel.get_kernel_path(None)) + '~')
-        os.remove(build_kernel.conf.get('main', 'grub_conf') + '~~')
-        os.remove(build_kernel.conf.get('main', 'grub_conf') + '~')
-
-    def test_run_external_tool(self):
-        build_kernel.run_external_tool()
+        test_process_for_given_version()
+        # force kernel version
+        test_process_for_given_version('linux-gentoo-test-v1')
+        test_process_for_given_version('linux-gentoo-test-v2')
+        test_process_for_given_version('linux-gentoo-test-v3')
+        test_process_for_given_version('linux-gentoo-test-v1')
 
 def get_unique_version(version):
     import random
     return version + str(random.random())
 
 if __name__ == "__main__":
-#    sys.argv = ['', 'Test.test_main']
-#    sys.argv.append('Test.test_load_config')
     unittest.main()
